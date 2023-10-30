1e206ea1e57f20890c9551c85aa29bd142f6dc6c
ThiccPan <ihilmiderian@gmail.com>
2023-10-30 11:21:21 +0700

    fix: add auth middleware to laporan route

--- routes/laporan.php
+++ routes/laporan.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\LaporanController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('laporan/tambah', [LaporanController::class, 'addTransaksiPage']);
-
-Route::post('laporan/tambah', [LaporanController::class, 'tambahLaporan'])->name('laporan.kirim');
-
-Route::get('laporan', [LaporanController::class, 'listLaporan'])->name('laporan.list');
+Route::middleware('auth')->group(function () {
+    Route::get('laporan/tambah', [LaporanController::class, 'addTransaksiPage']);
+    Route::post('laporan/tambah', [LaporanController::class, 'tambahLaporan'])->name('laporan.kirim');
+    Route::get('laporan', [LaporanController::class, 'listLaporan'])->name('laporan.daftar');
+});
