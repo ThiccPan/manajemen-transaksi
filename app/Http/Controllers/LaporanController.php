cdcf0f7109cad157e466b74c6ff9f5185e74ab35
ThiccPan <ihilmiderian@gmail.com>
2023-11-01 12:42:32 +0700

    feat: add laporan resource authorization access

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -2,32 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Laporan;
 use App\Services\LaporanService;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class LaporanController extends Controller
 {
-    public function AddTransaksiPage()
+    public function AddTransaksiPage(Request $request)
     {
+        ddd($request->user()->isAdmin());
         return view('tambah-laporan');
     }
 
     public function tambahLaporan(Request $request, Authenticatable $user)
     {
         $laporanBaru = (new LaporanService())->addLaporan($request, $user->id);
-        ddd($laporanBaru);
+        // ddd($laporanBaru);
+        return redirect(route('laporan.daftar'));
     }
 
     public function listLaporan(Authenticatable $user)
     {
-        $laporanData = (new LaporanService())->getAllLaporan($user->id, 5);
-        // ddd($laporanData);
+        $laporanData = [];
+        // TODO: refactor authorization handling
+        if ($user->divisi_id == 2) {
+            $laporanData = (new LaporanService())->getAllLaporan(5);
+        } else {
+            $laporanData = (new LaporanService())->getAllLaporanUser($user->id, 5);
+        }
         return view('list-laporan', [
             "daftar_laporan" => $laporanData,
         ]);
-        
+    }
+
+    public function detailLaporan(
+        Request $request,
+        string $laporanId,
+    ) {
+        $user = $request->user();
+        $dataLaporan = (new LaporanService())
+            ->getLaporanById($laporanId);
+
+        // TODO: refactor authorization handling
+        if (
+            $user->id != $dataLaporan->user_id
+            && $user->isAdmin()
+        ) {
+            return redirect(route('laporan.daftar'));
+        }
+
+        return view('detail-laporan', [
+            'laporan' => $dataLaporan,
+        ]);
     }
 }
