a5718aa9d0d40136ba7df01c4de2afc969394858
ThiccPan <ihilmiderian@gmail.com>
2023-11-08 21:24:51 +0700

    fix: auto inject controller dependencies

--- routes/laporan.php
+++ routes/laporan.php
@@ -1,15 +1,11 @@
 <?php
 
 use App\Http\Controllers\LaporanController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::get('laporan/tambah', [LaporanController::class, 'addTransaksiPage']);
     Route::post('laporan/tambah', [LaporanController::class, 'tambahLaporan'])->name('laporan.kirim');
     Route::get('laporan', [LaporanController::class, 'listLaporan'])->name('laporan.daftar');
-    Route::get('laporan/detail/{id}', function (Request $request, string $id) {
-        return (new LaporanController)->detailLaporan($request, $id, Auth::user());
-    })->name('laporan.detail');
+    Route::get('laporan/detail/{id}', [LaporanController::class, 'detailLaporan'])->name('laporan.detail');
 });
