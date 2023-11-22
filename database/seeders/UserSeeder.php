bed9b942457246d6b6c6a491b66cb478d3530810
ThiccPan <ihilmiderian@gmail.com>
2023-11-22 09:57:32 +0700

    feat: user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Ramsey\Uuid\Uuid;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'sales2',
+                'email' => 'sales2',
+                'password' => Hash::make('password'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'divisi_id' => '1',
+            ],
+            [
+                'name' => 'admin',
+                'email' => env("ADMIN_EMAIL"),
+                'password' => Hash::make(env("ADMIN_PASSWORD")),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'divisi_id' => '2',
+            ]
+        ]);
+    }
+}
