d91e209f5798404a3950a92ed6f88f05067d76aa
ThiccPan <ihilmiderian@gmail.com>
2023-12-02 17:46:54 +0700

    feat: chage ui to flowbite

--- resources/views/list-laporan.blade.php
+++ resources/views/list-laporan.blade.php
@@ -8,49 +8,98 @@
     <div class="mx-8 py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mb-12">
-                <x-bladewind::button icon="plus" button_text_css="font-bold" radius="small" tag="a"
-                    href="{{ route('laporan.tambah') }}">Tambah Laporan</x-bladewind::button>
+                <a href="{{ route('laporan.tambah') }}" type="button"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Tambah Laporan
+                    <svg class="w-3 h-3 ms-2 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 18 18">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9 1v16M1 9h16" />
+                    </svg>
+                </a>
             </div>
-            <x-bladewind.table>
-                <x-slot name="header">
-                    <th>ID</th>
-                    <th>Sales</th>
-                    <th>Lokasi mitra</th>
-                    <th>Dokumen</th>
-                    <th>Tanggal dibuat</th>
-                    <th>Ubah</th>
-                </x-slot>
-                @foreach ($daftar_laporan as $laporan)
-                    <tr>
-                        <td>{{ $laporan->id }}</td>
-                        <td>{{ $laporan->user->name }}</td>
-                        <td>
-                            <a href="{{ $laporan->url_koordinat }}" target="_blank"
-                                class="text-blue-600 visited:text-purple-600">
-                                Link
-                            </a>
-                        </td>
-                        <td>
-                            <a href="{{ asset('storage/' . $laporan->dokumen) }}" target="_blank"
-                                class="text-blue-600 visited:text-purple-600">
-                                Link
-                            </a>
-                        </td>
-                        <td>
-                            {{ $laporan->created_at }}
-                        </td>
-                        <td>
-                            <div class="flex flex-row">
-                                <form action="{{ route('laporan.detail', ['id' => $laporan->id]) }}" method="get">
-                                    <x-bladewind.button size="tiny" radius="small" can_submit="true" color="blue">
-                                        ubah
-                                    </x-bladewind.button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-            </x-bladewind.table>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="p-4">
+                                <div class="flex items-center">
+                                    <input id="checkbox-all-search" type="checkbox"
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                    <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                                </div>
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                No.
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                ID
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Sales
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Lokasi mitra
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Dokumen
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Tanggal dibuat
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Aksi
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($daftar_laporan as $laporan)
+                            <tr
+                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td class="w-4 p-4">
+                                    <div class="flex items-center">
+                                        <input id="checkbox-table-search-1" type="checkbox"
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                    </div>
+                                </td>
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $loop->index }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{ $laporan->id }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $laporan->user->name }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="{{ $laporan->url_koordinat }}" target="_blank"
+                                        class="text-blue-600 visited:text-purple-600">
+                                        Link
+                                    </a>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="{{ asset('storage/' . $laporan->dokumen) }}" target="_blank"
+                                        class="text-blue-600 visited:text-purple-600">
+                                        Link
+                                    </a>
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $laporan->created_at }}
+                                </td>
+                                <td class="flex items-center px-6 py-4">
+                                    <a href="{{ route('laporan.detail', ['id' => $laporan->id]) }}"
+                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <a href="#"
+                                        class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Remove</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
