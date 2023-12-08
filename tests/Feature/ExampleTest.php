8307439b2111f35139d4dc4551dc26ec89b8c343
ThiccPan <ihilmiderian@gmail.com>
2023-12-08 17:44:02 +0700

    feat: add test for testing env var

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,14 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Test if the environment is correct
+     */
+    public function test_correct_testing_env(): void
+    {
+        self::assertNotEquals("db_manajemen_transaksi", env('DB_DATABASE'));
     }
 }
