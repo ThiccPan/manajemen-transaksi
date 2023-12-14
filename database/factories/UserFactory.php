5e7e416e2c40e6a6f448683f832bde93abfb3079
ThiccPan <ihilmiderian@gmail.com>
2023-12-14 13:13:33 +0700

    fix: delete divisi seeder from userfactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,6 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        (new DivisiSeeder)->run();
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
