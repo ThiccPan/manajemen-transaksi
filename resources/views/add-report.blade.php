a13bdc52aceaccfc7443a8ac12e94ac74214b437
ThiccPan <ihilmiderian@gmail.com>
2023-12-18 16:45:35 +0700

    fix: add report blade page

--- resources/views/add-report.blade.php
+++ resources/views/add-report.blade.php
@@ -3,13 +3,9 @@
         class="block p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 my-4 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <form method="post" enctype="multipart/form-data" class="signup-form" action="{{ route('report.insert') }}">
             @csrf
-            <h1 class="my-2 text-2xl font-light text-gray-900">Tambah Laporan transaksi baru</h1>
-
-            <input type="hidden" name="coordinate" id="coordinate">
-
-
-            <label for="type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
-                option</label>
+            <h1 class="my-2 text-2xl font-light text-gray-900 mb-8">Tambah Laporan transaksi baru</h1>
+            <label for="type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipe laporan:
+            </label>
             <select id="type" name="type"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4">
                 <option value="VISIT">VISIT</option>
@@ -30,7 +26,9 @@
                 <a id="map-link" target="_blank" class="text-blue-600 visited:text-purple-600"></a>
             </p>
 
-            <button type="submit" name="btn-save"
+            <input type="hidden" name="coordinate" id="coordinate">
+
+            <button type="submit"
                 class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Unggah
                 laporan</button>
         </form>
@@ -60,7 +58,5 @@
                 console.log(err, fileItem.getMetadata('resize'));
             },
         });
-
-        const checkInPond = FilePond.create();
     </script>
 </x-app-layout>
