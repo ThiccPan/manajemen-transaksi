38cf53edbd2bf3615b9e698048f9ba6a2f8d280a
ThiccPan <ihilmiderian@gmail.com>
2023-12-21 16:43:26 +0700

    fix: unique constraint to report orders table report id

--- database/migrations/2023_12_14_150636_create_report_orders_table.php
+++ database/migrations/2023_12_14_150636_create_report_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('report_orders', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->uuid('report_id');
+            $table->uuid('report_id')->unique();
             $table->foreign('report_id')->references('id')->on('reports');
             $table->longText('notes');
             $table->decimal('price', 10, 2);
