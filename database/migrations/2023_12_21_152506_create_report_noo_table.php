45bcb41f548ad130c103b03379f05c81a7c584e3
ThiccPan <ihilmiderian@gmail.com>
2023-12-23 13:58:24 +0700

    fix: NOO table unset primary key

--- database/migrations/2023_12_21_152506_create_report_noo_table.php
+++ database/migrations/2023_12_21_152506_create_report_noo_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('report_noo', function (Blueprint $table) {
-            $table->uuid();
+            $table->uuid('id')->primary();
             $table->uuid('report_id')->unique();
             $table->foreign('report_id')->references('id')->on('reports');
             $table->timestamps();
