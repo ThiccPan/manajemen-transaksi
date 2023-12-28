4b1d8f014bd7b4dafab73df4b544959165c4d3db
ThiccPan <ihilmiderian@gmail.com>
2023-12-28 09:03:53 +0700

    feat: display last report

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,20 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-6">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __('Anda berhasil log in!') }}
+                </div>
+            </div>
+
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Anda berhasil log in!") }}
+                    Laporan terakhir:
+                    <p>id: {{ $last_report->id }}</p>
+                    <p>toko: {{ $last_report->client_name }}</p>
+                    <p>domisili: {{ $last_report->client_domicile }}</p>
+                    <p>status: {{ $last_report->status }}</p>
+                    <a href="{{ route('report.detail', $last_report->id) }}">detail</a>
                 </div>
             </div>
         </div>
